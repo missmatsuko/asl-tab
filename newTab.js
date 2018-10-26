3ffa707a6656a9b897d82df7354459e3b0411215
Matsuko Friedland <info@matsuko.ca>
2018-10-26 02:46:04 -0700

    After iframe API loads, switch from playlist to single video embed

--- newTab.js
+++ newTab.js
@@ -12,8 +12,9 @@ const ENDPOINT_PARAMS_ARRAY = Object.entries(ENDPOINT_PARAMS).map(([key, value])
   return `${key}=${encodeURIComponent(value)}`;
 });
 
-// Embed element
-const embedElement = document.getElementById('youtube');
+// iframe element
+const iframeId = 'iframe';
+const iframeElement = document.getElementById(iframeId);
 
 // Set up endpoint URL
 const url = `${ENDPOINT_BASE_URL}?${ENDPOINT_PARAMS_ARRAY.join('&')}`;
@@ -26,9 +27,43 @@ fetch(url)
     // Get the number of videos in the playlist
     const numVideos = data.items[0].contentDetails.itemCount;
 
-    // Set embed to play random video within playlist range
+    // Set iframe to play random video within playlist range
     const randomVideoIndex = Math.floor(Math.random() * numVideos);
-    embedElement.src += `&index=${ randomVideoIndex }`;
+    iframeElement.src += `&index=${ randomVideoIndex }`;
+
+    setTimeout(() => {
+      // Change playlist iframe to single video iframe
+      youtubeIframeApiPromise.then(() => {
+        const player = new YT.Player(iframeId, {
+          events: {
+            onReady: () => {
+              // Change playlist iframe to single video iframe
+              const videoUrl = player.getVideoUrl();
+              const parsedVideoUrl = new URL(videoUrl);
+              const videoId = parsedVideoUrl.searchParams.get('v');
+
+              player.loadVideoById(videoId);
+              player.playVideo();
+            },
+            onStateChange: (event) => {
+              // Fake loop the video
+              if (event.data === YT.PlayerState.ENDED) {
+                player.playVideo();
+              }
+            }
+          }
+        });
+      });
+    });
+
   }
 })
 .catch(error => console.error('Error:', error));
+
+// Create promise for onYouTubeIframeAPIReady
+const youtubeIframeApiPromise = new Promise((resolve, reject) => {
+  window.onYouTubeIframeAPIReady = function() {
+    resolve();
+    delete window.onYouTubeIframeAPIReady;
+  }
+});
